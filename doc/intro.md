93ec331e6db152ee2e31eed7651ae2c72fe0f3fd
J. Pablo Fernández <pupeno@carouselapps.com>
2015-07-15 09:48:10 +0100

    Created project: lein new to-jdbc-uri

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to to-jdbc-uri
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/to_jdbc_uri/core_test.clj
@@ -0,0 +1,7 @@
+(ns to-jdbc-uri.core-test
+  (:require [clojure.test :refer :all]
+            [to-jdbc-uri.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
