3344fadab40d9535611faae1645c6e01a9b9ba5b
J. Pablo Fernández <pupeno@carouselapps.com>
2015-07-19 10:38:41 +0100

    Pointed to the correct website for this library now that it has a repo.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 
 (defproject to-jdbc-uri "0.2.0-SNAPSHOT"
   :description "Simple function to convert non-JDBC URIs into JDBC ones"
-  :url "https://carouselapps.com"
+  :url "https://github.com/carouselapps/to-jdbc-uri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
