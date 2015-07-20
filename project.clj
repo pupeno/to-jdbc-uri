81fb208bc15e3eb7d9a1d8339fb4218dd5eb604e
J. Pablo Fernández <pupeno@carouselapps.com>
2015-07-20 08:06:07 +0100

    Attempt at having a better deployment workflow.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
   :url "https://github.com/carouselapps/to-jdbc-uri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:deploy-via :clojars}
+  :signing {:gpg-key "F2FB1C6F"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
