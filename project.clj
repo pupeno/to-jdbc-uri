1b95f02b72d937a0cdd814bdf73cf4b35d2789cc
J. Pablo Fernández <pupeno@carouselapps.com>
2015-07-20 08:06:16 +0100

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd
 
-(defproject to-jdbc-uri "0.2.0-SNAPSHOT"
+(defproject to-jdbc-uri "0.2.0"
   :description "Simple function to convert non-JDBC URIs into JDBC ones"
   :url "https://github.com/carouselapps/to-jdbc-uri"
   :license {:name "Eclipse Public License"
